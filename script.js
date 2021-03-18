c3c08b05c705a78e77835c4120479df372f16a17
Simon Žižek <spymon12@gmail.com>
2021-03-18 13:50:33 +0100

    add transparent background for buttons and change font size to 17px to get smootherer transition

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ const questionBx = document.querySelectorAll('.questionBx')
 const box = document.getElementById('box')
 questionBx.forEach(e => {
   e.addEventListener('click', function () {
+    /* let arr = Array.from(questionBx).forEach(x => x.classList.remove('show')) */
+    /*.filter(x => x.classList.contains('show'))
+     */
+
+    /* this.classList.contains('show')
+      ? this.classList.remove('show')
+      : this.classList.add('show') */
     this.classList.toggle('show')
+
     box.style.animation = 'jump 300ms forwards'
     setTimeout(() => {
       box.style.animation = ''
